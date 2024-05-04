912d46d30c5fbc49c68c1adb1f860150a60c44e6
Jimmie Houts <jimmie@teamredwall.com>
2024-05-03 21:30:41 -0700

    Initial commit Projects created from Aspire template Using Octokit to pull in Github repos

---
+++ MercyFork.AppHost/Program.cs
@@ -0,0 +1,9 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.MercyFork_ApiService>("apiservice");
+
+builder.AddProject<Projects.MercyFork_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+builder.Build().Run();
